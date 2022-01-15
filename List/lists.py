f17ce3e16b8bc7b7d7f202b38b2aacc8ccb3310c
DarylTay0806 <85399718+DarylTay0806@users.noreply.github.com>
2022-01-15 22:19:52 +0800

    Update lists.py

--- List/lists.py
+++ List/lists.py
@@ -5,7 +5,7 @@ parrot_list = ["non pinin'", "no more", "a stiff", "bereft of live"]
 parrot_list.append("A Norwegian Blue")
 
 for state in parrot_list:
-    print("This parrot is " + state)
+    print("This parrots are " + state)
 
 
 even = [2, 4, 6, 8]
